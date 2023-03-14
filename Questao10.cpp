443466ea66e4348222ef4f4f413cb4c8211454c7
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2023-03-14 13:17:13 -0300

    Add files via upload

---
+++ Questao10.cpp
@@ -0,0 +1,54 @@
+/*dada a matriz A abaixo, calcule a matriz de reflexão em relação ao eixo y
+A = [1,2]
+	[3,4}
+na matriz de reflexao as colunas são invertidas, logo 1, 2 passa a ser 2,1
+*/
+#include <stdio.h>
+#include <locale.h>
+#define T_LIN 2
+#define T_COL 2
+
+int main(){
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	
+	printf("Calculando a matriz de reflexão em relação ao eixo y\n\n");
+	
+	int mat[T_LIN][T_COL];
+	int reflexao[T_LIN][T_COL];
+
+	for(int i = 0; i<T_LIN; i++){//informar os valores da matriz
+		for(int j=0; j<T_COL; j++){
+			printf("Informe o valor[[%d][%d]: ", i, j);
+			scanf("%d", &mat[i][j]);
+		}	
+	}
+	
+	printf("\nMatriz Original: \n");//mostra a matriz com seus respectivos valores 
+	for(int i = 0; i<T_LIN; ++i){
+		for(int j = 0; j<T_COL; ++j){
+			printf("%d\t", mat[i][j]);
+		}
+		printf("\n");
+	}
+		
+	for(int i = 0; i < T_LIN; i++){//a nova matriz criada reflexao, vai guardar os valores da matriz de forma inversa
+       		for(int j = 0; j < T_COL; j++){//ex: quando j for 1, T-COL - 1 - 1, logo 0; se j = 0, fica T_COL - 1 - 0, logo 1.
+           		if(j == 0) {
+	    			reflexao[i][j] = -mat[i][j];	
+			} else {
+				reflexao[i][j] = mat[i][j];
+			}
+	   	}
+	}
+
+    printf("\nMatriz Reflexão: \n");
+    for(int i = 0; i < T_LIN; i++){
+        for(int j = 0; j < T_COL; j++){
+            printf("%d\t", reflexao[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
